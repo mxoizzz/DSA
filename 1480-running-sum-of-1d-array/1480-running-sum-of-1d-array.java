c0ed713e57e5b2ce07ada6e50d94e3da0191bb0b
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-12 02:26:26 +0530

    Time: 0 ms (100%), Space: 44.3 MB (35.65%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,15 +1,9 @@
 class Solution {
-    public int sum(int n,int[] nums){
-        int sum=0;
-        for(int i =0;i<=n;i++){
-            sum+=nums[i];
-        }   
-        return sum;
-    }
     public int[] runningSum(int[] nums) {
         int[] runningSum = new int[nums.length];
-        for(int i =0;i<nums.length;i++){
-            runningSum[i] = sum(i,nums); 
+        runningSum[0] = nums[0];
+        for(int i =1;i<nums.length;i++){
+            runningSum[i] = runningSum[i-1] + nums[i]; 
         }
         return runningSum;
     }
