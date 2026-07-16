92cf08ee2f914731e780ff816ea65e00b30abcdd
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-16 14:27:44 +0530

    Time: 25 ms (14.93%), Space: 81.3 MB (81.76%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int i =0;
+        while(i<nums.length-1){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+}
