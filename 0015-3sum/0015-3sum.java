dd754779cc0b835ae2670e608d3fcee5e51f29f8
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-26 17:55:27 +0530

    Time: 33 ms (73.59%), Space: 60.2 MB (12.06%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,60 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0;i<n-2;i++){
+            if(i>0 && nums[i] == nums[i-1]){continue;}
+            int left = i+1;
+            int right = n-1;
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    List<Integer> triplets = new ArrayList<>();
+                    triplets.add(nums[i]);
+                    triplets.add(nums[left]);
+                    triplets.add(nums[right]);
+                    res.add(triplets);
+                    left++;
+                    right--;
+                    while(left<n && nums[left] == nums[left-1]){
+                        left++;
+                    }
+                    while(right>=0 && nums[right] == nums[right+1]){
+                        right--;
+                    }
+                }
+                else if(sum<0){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }
+        return res;
+
+        //Brute Force (311/316 passed)
+
+        // Set<List<Integer>> triplets = new HashSet<>();
+        // for(int i =0;i<nums.length;i++){
+        //     for(int j =i+1;j<nums.length;j++){
+        //         for(int k =j+1;k<nums.length;k++){
+        //             if((i!=j && i!=k && k!=j) && nums[i] + nums[j] + nums[k] == 0){
+        //                 List<Integer> row = new ArrayList<>();
+        //                 row.add(nums[i]);
+        //                 row.add(nums[j]);
+        //                 row.add(nums[k]);
+        //                 Collections.sort(row);
+        //                 triplets.add(row);
+        //             }
+        //         }
+        //     }
+        // }
+        // List<List<Integer>> res = new ArrayList<>();
+        // for(List<Integer> lst : triplets){
+        //     res.add(lst);
+        // }
+        // return res;
+    }
+}
