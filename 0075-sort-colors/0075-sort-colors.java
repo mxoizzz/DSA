b66ce40bf3c799b6cd970c3ad87f4dd002d916b5
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-26 23:42:02 +0530

    Time: 0 ms (100%), Space: 43.3 MB (78.06%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,26 @@
 class Solution {
+    public void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int low = 0,
+            mid = 0,
+            high = nums.length-1;
+        while(mid<=high){
+            if(nums[mid] == 0){
+                swap(nums,low,mid);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 2){
+                swap(nums,high,mid);
+                high--;
+            }
+            else{
+                mid++;
+            }
+        }
     }
 }
