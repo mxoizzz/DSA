d263a17e49046ba6cf7b5919791b1d405e04424c
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-07-26 18:00:56 +0530

    Time: 32 ms (78.67%), Space: 60.6 MB (5.8%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,17 +4,17 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         for(int i = 0;i<n-2;i++){
-            if(i>0 && nums[i] == nums[i-1]){continue;}
+            if(i>0 && nums[i] == nums[i-1])continue;
             int left = i+1;
             int right = n-1;
             while(left<right){
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum == 0){
-                    List<Integer> triplets = new ArrayList<>();
-                    triplets.add(nums[i]);
-                    triplets.add(nums[left]);
-                    triplets.add(nums[right]);
-                    res.add(triplets);
+                    List<Integer> triplet = new ArrayList<>();
+                    triplet.add(nums[i]);
+                    triplet.add(nums[left]);
+                    triplet.add(nums[right]);
+                    res.add(triplet);
                     left++;
                     right--;
                     while(left<n && nums[left] == nums[left-1]){
