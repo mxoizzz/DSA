6663483c693288fbdca9f34344fbcb97e5c2c648
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-08-13 13:15:33 +0530

    Time: 7 ms (14.7%), Space: 45.1 MB (24.96%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        return Arrays.stream(nums2).filter(set1::contains).distinct().toArray();
+    }
+}
