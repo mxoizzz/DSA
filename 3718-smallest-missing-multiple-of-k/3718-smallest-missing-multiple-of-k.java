b5adcbc15605ae618332b0d55e2e454407689106
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-08-25 23:33:26 +0530

    Time: 2 ms (68.2%), Space: 45.5 MB (26.41%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,5 +1,9 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
+        if (k == 0) {
+            return 0; 
+        }
+
         HashSet<Integer> set = new HashSet<>();
         for (int num : nums) {
             set.add(num);
