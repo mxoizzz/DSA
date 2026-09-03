c377fcb56065b1650294769df2ac9c3dc37d1e2f
Moiz Shaikh <moiz.shaikh32477@gmail.com>
2026-09-03 21:41:24 +0530

    Time: 0 ms (100%), Space: 12.5 MB (5.82%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,24 +12,30 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        TreeNode* T = root;
-        stack<TreeNode*> S;
+        // TreeNode* T = root;
+    //     stack<TreeNode*> S;
         vector<int> res;
-        while(T!=NULL){
-            S.push(T);
-            T = T->left;
-        }
-        while(!S.empty()){
-            T = S.top();
-            S.pop();
-            res.push_back(T->val);
-            T = T->right;
-            while(T!=NULL){
-                S.push(T);
-                T = T->left;
-            }
-        }
+    //     while(T!=NULL){
+    //         S.push(T);
+    //         T = T->left;
+    //     }
+    //     while(!S.empty()){
+    //         T = S.top();
+    //         S.pop();
+    //         res.push_back(T->val);
+    //         T = T->right;
+    //         while(T!=NULL){
+    //             S.push(T);
+    //             T = T->left;
+    //         }
+    //     }
+    //     return res;
+        if (root == nullptr) return res;
+        vector<int> leftTree = inorderTraversal(root->left);
+        res.insert(res.end(),leftTree.begin(),leftTree.end());
+        res.push_back(root->val);
+        vector<int> rightTree = inorderTraversal(root->right);
+        res.insert(res.end(),rightTree.begin(),rightTree.end());
         return res;
-
     }
 };
